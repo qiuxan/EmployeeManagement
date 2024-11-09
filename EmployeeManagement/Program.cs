8fd1d4d4017ac0560e6a94cc35837025f84eaed4
qiuxan <qiuxan@gmail.com>
2024-11-10 08:50:36 +1100

    Creating and registering a database context

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,10 +1,22 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
 namespace EmployeeManagement;
+
 
 public class Program
 {
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseInMemoryDatabase("EmployeeDb");
+        });
+
         var app = builder.Build();
 
         app.MapGet("/", () => "Hello World!");
