109028283a4e0b396f9c84d2b3c7f9f342e61cdf
qiuxan <qiuxan@gmail.com>
2024-11-10 10:50:43 +1100

    Using the repository design pattern and creating the repository interface

---
+++ EmployeeManagement/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeManagement.Model;
+
+namespace EmployeeManagement.Repositories;
+
+public interface IEmployeeRepository
+{
+    Task<IEnumerable<Employee>> GetAllAsync();
+    Task<Employee> GetByIdAsync(int id);
+    Task AddEmployeeAsync(Employee employee);
+    Task UpdateEmployeeAsync(Employee employeeChanges);
+    Task DeleteEmployeeAsync(int id);
+}
