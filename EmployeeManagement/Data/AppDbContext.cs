e994f4d09d7e93edb304d9fa31f42d387a1c2704
qiuxan <qiuxan@gmail.com>
2024-11-10 08:58:18 +1100

    Configuring CORS for Angular

--- EmployeeManagement/Data/AppDbContext.cs
+++ EmployeeManagement/Data/AppDbContext.cs
@@ -5,11 +5,12 @@ namespace EmployeeManagement.Data;
 
 public class AppDbContext:DbContext
 {
+    public DbSet<Employee> Employees { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options) 
         : base(options)
     {
 
     }
 
-    public DbSet<Employee> Employees { get; set; }
+    
 }
--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -12,12 +12,27 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("MyCors", builder =>
+            {
+                builder
+                .WithOrigins("http://localhost:4200")// Angular App URL
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            });
+        });
+
+
         builder.Services.AddDbContext<AppDbContext>(options =>
         {
             options.UseInMemoryDatabase("EmployeeDb");
         });
 
         var app = builder.Build();
+
+        app.UseCors("MyCors");
 
         app.MapGet("/", () => "Hello World!");
 
