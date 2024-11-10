ea5351ad5d00e910e2f88f44809c2989dee5191a
qiuxan <qiuxan@gmail.com>
2024-11-11 06:38:27 +1100

    Creating the employee API conroller with a create endpoint

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeManagement.Model;
+using EmployeeManagement.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EmployeeController: ControllerBase
+{
+    private readonly IEmployeeRepository _employeeRepository;
+    public EmployeeController(IEmployeeRepository employeeRepository)
+    {
+        _employeeRepository = employeeRepository;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+    {
+       await _employeeRepository.AddEmployeeAsync(employee);
+        return Created();
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Employee>> GetEmployees()
+    {
+        return await _employeeRepository.GetAllAsync();
+    }
+}
