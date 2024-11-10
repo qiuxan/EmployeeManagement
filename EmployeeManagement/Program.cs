7e6145dca782a6eefd08d1c1ac5849167a824d55
qiuxan <qiuxan@gmail.com>
2024-11-11 07:39:44 +1100

    Adding and configuring Swagger

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -36,11 +36,24 @@ public class Program
 
         builder.Services.AddControllers();
 
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
         var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        { 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => 
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
 
         app.UseCors("MyCors");
 
-        app.MapGet("/", () => "Hello World!");
+        app.MapControllers();
 
         app.Run();
     }
