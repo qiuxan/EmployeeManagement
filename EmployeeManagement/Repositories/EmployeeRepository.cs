41c841e0331b6998399688eb8026553bb4e0107c
qiuxan <qiuxan@gmail.com>
2024-11-10 14:13:35 +1100

    Creating the employee repository class

---
+++ EmployeeManagement/Repositories/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Model;
+
+namespace EmployeeManagement.Repositories;
+
+public class EmployeeRepository : IEmployeeRepository
+{
+    private readonly AppDbContext _context;
+    public EmployeeRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    public async Task AddEmployeeAsync(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteEmployeeAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Employee>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Employee> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateEmployeeAsync(Employee employeeChanges)
+    {
+        throw new NotImplementedException();
+    }
+}
