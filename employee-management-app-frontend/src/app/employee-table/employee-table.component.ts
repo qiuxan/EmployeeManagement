a79953ab3f4aff82103b5bb03e6c9f1da97e50e4
qiuxan <qiuxan@gmail.com>
2024-11-17 07:49:50 +1100

    Showing the employee data in a table

--- employee-management-app-frontend/src/app/employee-table/employee-table.component.ts
+++ employee-management-app-frontend/src/app/employee-table/employee-table.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from '../employee.service';
 import { Employee } from '../../models/employee';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'employee-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.css'
 })
