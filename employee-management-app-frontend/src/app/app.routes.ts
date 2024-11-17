6a907732f2072de4614d8f1de859a0ac5011a8b8
qiuxan <qiuxan@gmail.com>
2024-11-17 13:07:52 +1100

    Defining routes in angular

--- employee-management-app-frontend/src/app/app.routes.ts
+++ employee-management-app-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { EmployeeTableComponent } from './employee-table/employee-table.component';
+
+export const routes: Routes = [
+    {path:'',component:EmployeeTableComponent},
+    {'path':'create',component:EmployeeFormComponent},
+    {'path':'employee',redirectTo:'',pathMatch:'full'},
+];
